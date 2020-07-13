4fd46128b9e592295e5b7fa64c2cd482ab362cde
Adarsh Chaudhary <adarshchaudhary434@gmail.com>
2020-07-13 12:04:40 +0530

    first commit

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 const TODO = require('../models/todo');
-module.exports.accessUser= function(req, res) {
+
+//access details from the database, using ejs template engine, render details in the browser 
+module.exports.accessUser = function(req, res) {
 
     TODO.find({}, function(err, todos) {
 
@@ -14,35 +16,36 @@ module.exports.accessUser= function(req, res) {
     });
 };
 
-module.exports.createUser= function(req, res) {
+//create details and store details in database
+module.exports.createUser = function(req, res) {
 
-        TODO.create({
-            description: req.body.description,
-            catagory: req.body.catagory,
-            dueDate: req.body.dueDate
-        }, function(err, newTODO) {
+    TODO.create({
+        description: req.body.description,
+        catagory: req.body.catagory,
+        dueDate: req.body.dueDate
+    }, function(err, newTODO) {
 
-            if (err) {
-                console.log("Error in creating a task");
-                return;
-            } else {
-                console.log("*************", newTODO);
-                return res.redirect('back');
-            }
-        });
-
-    };
-
-
-
-module.exports.deleteUser=function(req, res) {
-
-        let id = req.query.id;
-        TODO.findByIdAndDelete(id, function(err) {
-            if (err) {
-                console.log("Error in deleting an object from database");
-                return;
-            }
+        if (err) {
+            console.log("Error in creating a task");
+            return;
+        } else {
+            console.log("*************", newTODO);
             return res.redirect('back');
-        });
-    }
+        }
+    });
+
+};
+
+
+//remove details from the database 
+module.exports.deleteUser = function(req, res) {
+
+    let id = req.query.id;
+    TODO.findByIdAndDelete(id, function(err) {
+        if (err) {
+            console.log("Error in deleting an object from database");
+            return;
+        }
+        return res.redirect('back');
+    });
+}
